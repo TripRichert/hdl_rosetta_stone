c0e58cdb1743916d58e6686435794cc6ffe6594e
Trip Richert <triprichert@users.noreply.github.com>
2023-09-15 04:21:28 -0400

    added checks for dependencies

--- run.py
+++ run.py
@@ -1,5 +1,25 @@
 import os
 import os.path
+import subprocess
+import importlib
+import importlib.util
+
+def which(program):
+    import os
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ.get("PATH", "").split(os.pathsep):
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
 
 def cpy_file(outputdir, name, inputfile, substitution):
     olddata = ""
@@ -27,7 +47,7 @@ def cpy_file(outputdir, name, inputfile, substitution):
 def prepare_cocotb_dir(outputdir, name, template_file, prjpath, source_files, simulator, language, pythonfile, topname):
     corrected_source_files = []
     if not prjpath:
-        prjpath = os.path.dirname(__file__)
+        prjpath = os.getcwd()
     for filename in source_files:
         if os.path.isabs(filename):
             corrected_source_files.append(filename)
@@ -52,6 +72,22 @@ def prepare_cocotb_dir(outputdir, name, template_file, prjpath, source_files, si
     cpy_file(os.path.join(outputdir, name), pyname + '.py', pythonfile, {})
     cpy_file(os.path.join(outputdir, name), 'makefile', template_file, vardict)
 
+def execute_cocotb_dir(dirname):
+    return subprocess.run(["make"], shell=True, capture_output=True, text=True, cwd=dirname)
+
+def translate_simulator(simulator):
+    if simulator == 'icarus':
+        return 'iverilog'
+    else:
+        return simulator
+def check_cocotb(simulator):
+    will_work = True
+    if which(translate_simulator(simulator)) == None:
+        will_work = False
+    if importlib.util.find_spec('cocotb') == None:
+        will_work = False
+    return will_work
+    
 mydir = os.path.dirname(os.path.realpath(__file__))
 source_files = [os.path.join('verilog', 'hdl', 'blockram.v'), os.path.join('verilog', 'hdl', 'bram_std_fifo.v')]
 prepare_cocotb_dir(outputdir = 'build',
@@ -65,6 +101,7 @@ prepare_cocotb_dir(outputdir = 'build',
                    topname='bram_std_fifo')
 
 source_files = [os.path.join('vhdl', 'hdl', 'blockram.vhdl'), os.path.join('vhdl', 'hdl', 'bram_std_fifo.vhdl')]
+
 prepare_cocotb_dir(outputdir = 'build',
                    name='vhdl_bram_std_fifo',
                    template_file=os.path.join('templates', 'makefile.in'),
@@ -74,3 +111,20 @@ prepare_cocotb_dir(outputdir = 'build',
                    language='vhdl',
                    pythonfile=os.path.join('cocotb_tests', 'test_bram_std_fifo.py'),
                    topname='bram_std_fifo')
+
+data1 = ''
+data2 = ''
+if check_cocotb('icarus'):
+    out1 = execute_cocotb_dir(os.path.join('build', 'verilog_bram_std_fifo'))
+    data1 = out1.stdout
+else:
+    print('cannot simulate verilog_bram_std_fifo without icarus')
+    
+if check_cocotb('ghdl'):
+    out2 = execute_cocotb_dir(os.path.join('build', 'vhdl_bram_std_fifo'))
+    data2 = out2.stdout
+else:
+    print('cannot simulate vhdl_bram_std_fifo without ghdl')
+
+print(data1)
+print(data2)
